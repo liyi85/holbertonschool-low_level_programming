70c146584826d486d54e8fbcf5411fe83f28174f
liyi85@gmail.com <liyi85@gmail.com>
2021-06-19 17:10:28 -0700

    100-print_comb3.c - Write a program that prints all possible different combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+* main - Prints numbers between 01 to 10.
+*
+*Description Numbers must be separated by coma followed by a space
+*Program that prints all possible different combinations of two digits
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int i, j;
+
+i = 48;
+j = 48;
+
+while (e < 58)
+{
+i = 48;
+while (i < 58)
+{
+if (j != i && j < i)
+{
+putchar(j);
+putchar(i);
+if (i == 57 && j == 56)
+{
+break;
+}
+putchar(',');
+putchar(' ');
+}
+i++;
+}
+j++;
+}
+putchar('\n');
+return (0);
+}
