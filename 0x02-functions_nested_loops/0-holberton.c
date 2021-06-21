35487c256829723486740ec4e39bcf52a91e1bfb
liyi85@gmail.com <liyi85@gmail.com>
2021-06-21 11:34:10 -0700

    0-holberton.c - print Holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+* main - Prints Holberton as a message.
+*Description
+*Program that prints Holberton
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+sizeh int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
+int count, sz;
+
+sizeh = sizeof(str) / sizeof(int);
+for (count = 0; count < sizeh; count++)
+{
+_putchar(str[count]);
+}
+_putchar('\n');
+return (0);
+}
