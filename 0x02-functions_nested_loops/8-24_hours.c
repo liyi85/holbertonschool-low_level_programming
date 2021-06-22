581654fd5d83917ab8c7e2294226b8201c3fb0d7
liyi85@gmail.com <liyi85@gmail.com>
2021-06-22 11:30:21 -0700

    8-24_hours.c - prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+* jack_bauer - Prints the minutes of a day
+*
+* Return: no return
+*/
+void jack_bauer(void)
+{
+int i, j, k, l;
+
+for (i = 48; i <= 50; i++)
+{
+for (j = 48; j <= 57; j++)
+{
+for (k = 48; k <= 53; k++)
+{
+for (l = 48; l <= 57; l++)
+{
+if (i >= 50 && j >= 52)
+break;
+_putchar(i);
+_putchar(j);
+_putchar(58);
+_putchar(k);
+_putchar(l);
+_putchar('\n');
+}
+}
+}
+}
+}
