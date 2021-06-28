4d61748c7f7a76ebd4bceb12d132b9e90f1bc39a
liyi85@gmail.com <liyi85@gmail.com>
2021-06-28 14:37:55 -0700

    2-strlen.c - returns the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+* _strlen - returns the length of a string.
+* @s: input string.
+* Return: length of a string.
+*/
+int _strlen(char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != 0; c++)
+	{
+	}
+	return (c);
+}
